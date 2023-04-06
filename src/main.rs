93116003e2bc39c667b2efe4bd0fdccbb6777feb
LIARA <liarabruchmann@gmail.com>
2023-04-06 16:08:10 +0200

    feat: poem rest api

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,34 @@
-fn main() {}
+use poem::{
+  endpoint::StaticFilesEndpoint,
+  get, handler,
+  listener::TcpListener,
+  middleware::{NormalizePath, TrailingSlash},
+  EndpointExt, Route, Server,
+};
+use sociare::util;
+
+#[handler]
+fn api() -> String {
+  format!("current_time: {}", util::unix_ms())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+  if std::env::var_os("RUST_LOG").is_none() {
+    std::env::set_var("RUST_LOG", "poem=debug");
+  }
+  tracing_subscriber::fmt::init();
+
+  let mut app = Route::new();
+
+  app = app.at("/api/v1", get(api));
+
+  app = app.at(
+    "/",
+    StaticFilesEndpoint::new("./www").index_file("index.html"),
+  );
+
+  Server::new(TcpListener::bind("127.0.0.1:3030"))
+    .run(app.with(NormalizePath::new(TrailingSlash::Trim)))
+    .await
+}
