c723860783037b3cbdbcccd66b74db17c8b2c8ee
LIARA <liarabruchmann@gmail.com>
2023-04-12 15:26:26 +0200

    feat: ported sveltekit landing page to vanilla svelte

--- vite/src/App.svelte
+++ vite/src/App.svelte
@@ -1,47 +1,203 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import './assets/app.scss'
+
+	import payve_logo from './assets/payve_logo.svg';
+	import Button from './components/Button.svelte';
+
+	import yen_banknote_3d from './assets/fluent_emoji/yen_banknote_3d.png';
+	import euro_banknote_3d from './assets/fluent_emoji/euro_banknote_3d.png';
+	import coin_3d from './assets/fluent_emoji/coin_3d.png';
+	import dollar_banknote_3d from './assets/fluent_emoji/dollar_banknote_3d.png';
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+	<div class="head">
+		<div class="floating-emojis">
+			<div class="left side">
+				<div class="emoji yen">
+					<img src={yen_banknote_3d} alt="Yen Emoji" />
+				</div>
+				<div class="emoji euro">
+					<img src={euro_banknote_3d} alt="Euro Emoji" />
+				</div>
+			</div>
+			<div class="right side">
+				<div class="emoji coin">
+					<img src={coin_3d} alt="Coin Emoji" />
+				</div>
+				<div class="emoji dollar">
+					<img src={dollar_banknote_3d} alt="Dollar Emoji" />
+				</div>
+			</div>
+		</div>
 
-  <div class="card">
-    <Counter />
-  </div>
+		<div class="nav">
+			<div class="container">
+				<div class="logo">
+					<img src={payve_logo} alt="payve_logo" />
+				</div>
+				<div class="links">
+					<a
+						href="https://github.com/alexampersandria/sociare"
+						target="_blank"
+						rel="noopener noreferrer"
+						class="alt none">GitHub</a
+					>
+				</div>
+			</div>
+		</div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+		<div class="call-to-action">
+			<div class="container">
+				<h1>
+					House Share Economy<br />
+					Made Easier
+				</h1>
+				<p class="explainer muted">
+					Payve is an open source tool that allows groups of people to easily manage their finances
+					and settle debts between them.
+				</p>
+				<div class="links">
+					<Button type={'primary'}>Get Started</Button>
+					<Button type={'hollow'}>GitHub</Button>
+				</div>
+			</div>
+		</div>
+	</div>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+	.head {
+		position: relative;
+		padding-bottom: 1rem;
+		border-bottom: #000 solid 2px;
+	}
+
+	.head::before {
+		inset: 0;
+		content: '';
+		display: block;
+		position: absolute;
+		z-index: -2;
+		background-color: hsla(0, 0%, 100%, 1);
+		background-image: radial-gradient(at 80% 61%, hsla(270, 47%, 72%, 1) 0px, transparent 50%),
+			radial-gradient(at 32% 61%, hsla(33, 100%, 80%, 1) 0px, transparent 50%),
+			radial-gradient(at 87% 20%, hsla(311, 98%, 75%, 1) 0px, transparent 50%),
+			radial-gradient(at 51% 3%, hsla(337, 72%, 74%, 1) 0px, transparent 50%),
+			radial-gradient(at 0% 0%, hsla(184, 50%, 52%, 1) 0px, transparent 50%);
+		background-size: 200% 200%;
+	}
+
+	.head::after {
+		inset: 0;
+		content: '';
+		display: block;
+		position: absolute;
+		z-index: -1;
+		background-color: transparent;
+		background-image: linear-gradient(transparent, #ffffff88),
+			linear-gradient(60deg, #ffffff66, transparent, transparent),
+			linear-gradient(-60deg, #ffffff66, transparent, transparent);
+	}
+
+	.nav,
+	.call-to-action {
+		position: relative;
+		z-index: 1;
+	}
+	.nav .logo img {
+		width: 100px;
+	}
+
+	.nav .container {
+		display: flex;
+		align-items: center;
+		padding-top: 1rem;
+	}
+
+	.nav .container .links,
+	.nav .container .logo {
+		flex: 1;
+	}
+
+	.nav .links {
+		text-align: right;
+	}
+
+	.container {
+		max-width: 960px;
+		margin: 0 auto;
+		padding: 0 1rem;
+	}
+
+	.call-to-action {
+		text-align: center;
+		padding: 2rem 0 6rem 0;
+	}
+
+	.call-to-action .explainer {
+		max-width: 400px;
+		margin: 0 auto;
+		margin-bottom: 2rem;
+	}
+
+	.call-to-action .links > :global(*) {
+		margin: 0 0.5em;
+	}
+
+	.floating-emojis {
+		max-width: 960px;
+		margin: 0 auto;
+		position: absolute;
+		inset: 0;
+		pointer-events: none;
+	}
+
+	.floating-emojis .side {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+	}
+
+	.floating-emojis .right.side .emoji {
+		right: 0;
+	}
+
+	.floating-emojis .emoji {
+		position: absolute;
+		top: 50%;
+		translate: 0 -50%;
+	}
+
+	.floating-emojis .emoji img {
+		width: 12rem;
+	}
+
+	.floating-emojis .yen {
+		transform: rotate(-12.5deg) translate(-6rem, -5rem);
+		filter: drop-shadow(-.5rem -.5rem 3rem #a7612c66);
+	}
+
+	.floating-emojis .yen img {
+		width: 11rem;
+	}
+
+	.floating-emojis .euro {
+		transform: rotate(25deg) translate(1rem, 1rem);
+		filter: drop-shadow(-.5rem -.5rem 3rem #17749d66);
+	}
+
+	.floating-emojis .coin {
+		transform: rotate(12.5deg) translate(-2rem, -2rem);
+		filter: drop-shadow(-1rem 0 3rem #be824366);
+	}
+
+	.floating-emojis .coin img {
+		width: 11rem;
+	}
+
+	.floating-emojis .dollar {
+		transform: rotate(-17.5deg) translate(6rem, 3rem);
+		filter: drop-shadow(-1rem 0 3rem #1e906166);
+	}
 </style>
---
+++ vite/src/components/Button.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	export let type: 'primary' | 'hollow' = 'primary';
+	export let disabled: boolean = false;
+	export let rounded: boolean = false;
+</script>
+
+<button class={type} class:rounded class:disabled>
+	<slot />
+</button>
+
+<style>
+	button {
+		border: 0;
+		background: #000;
+		color: #fff;
+		font-size: 1rem;
+		padding: 0.75em 1.5em;
+		border-radius: 0.25em;
+		cursor: pointer;
+		transition: background 0.25s ease-in-out;
+	}
+
+	button.primary {
+		background: #000;
+		color: #fff;
+	}
+
+	button.primary:not(.disabled):hover {
+		background: var(--neutral-800);
+	}
+
+	button.hollow {
+    position: relative;
+		background: transparent;
+		color: #000;
+	}
+
+	button.hollow::before {
+		content: '';
+		position: absolute;
+		inset: 0;
+		border-radius: 0.25em;
+		border: 0.15em solid #000;
+		clip-path: polygon(
+			0% 0%,
+			0% 100%,
+			0.5em 100%,
+			0.5em 0,
+			calc(100% - 0.5em) 0,
+			calc(100% - 0.5em) 100%,
+			100% 100%,
+			100% 0%
+		);
+		transition: clip-path 0.3s cubic-bezier(0.5, 0.05, 0.5, 0.95);
+	}
+
+	button.hollow:not(.disabled):hover::before {
+		clip-path: polygon(0% 0%, 0% 100%, 50% 100%, 50% 0, 50% 0, 50% 100%, 100% 100%, 100% 0%);
+	}
+
+	button.disabled {
+		opacity: 0.5;
+		cursor: not-allowed;
+	}
+
+	button.rounded {
+		border-radius: 9999em;
+	}
+</style>
