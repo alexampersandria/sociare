19381965381c159d0102b7bedd783c25897860a1
LIARA <liarabruchmann@gmail.com>
2023-04-19 22:02:14 +0200

    fix: markdown options

--- vite/src/components/MessageDown.svelte
+++ vite/src/components/MessageDown.svelte
@@ -8,9 +8,20 @@
 
 	export let source
 	export let isInline = false
+
+	let endtag = 'none'
+	$: {
+		if (source) {
+			let sections = source.split(' ')
+			let last_section = sections[sections.length - 1]
+			if (last_section.startsWith('/')) {
+				endtag = last_section.replace('/', '')
+			}
+		}
+	}
 </script>
 
-<div class="messagedown" class:isInline>
+<div class="messagedown endtag-{endtag}" class:isInline>
 	<SvelteMarkdown
 		{source}
 		{isInline}
@@ -19,6 +30,12 @@
 			code: Code,
 			image: Image,
 			link: Link,
+		}}
+		options={{
+			breaks: true,
+			tables: true,
+			gfm: true,
+			sanitize: true,
 		}}
 	/>
 </div>
@@ -35,13 +52,17 @@
 	.messagedown :global(p),
 	.messagedown :global(ul),
 	.messagedown :global(ol),
-	.messagedown :global(pre) {
+	.messagedown :global(pre),
+	.messagedown :global(blockquote) {
 		margin: 0;
 	}
-
 	.messagedown :global(code) {
 		padding: 0;
 		background: transparent;
 		color: var(--gray-600);
+	}
+
+	.messagedown.endtag-s {
+		font-family: 'Comic Sans MS', 'Comic Sans', cursive;
 	}
 </style>
